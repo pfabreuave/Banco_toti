eeb36abb92ef6e94b5590dad1ff7760e96b56545
Pedro Abreu <pfabreuave.gmail.com>
2023-03-01 12:32:57 -0300

    se corrige sql de representante

--- list_dependente.sql
+++ list_dependente.sql
@@ -9,4 +9,4 @@ FROM cliente_conta AS dep
 JOIN cliente_conta AS rep ON (dep.id_conta = rep.id_conta)
 JOIN cliente c ON (rep.id_cliente = c.id)
 WHERE dep.dependente = true
-ORDER BY rep.id_conta, GERARQUIA DESC;
+ORDER BY rep.id_conta DESC;
