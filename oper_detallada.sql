292f52a2b5c9a9cc853520dd1faba95cea87c0e5
Pedro Abreu <pfabreuave.gmail.com>
2023-03-04 16:28:09 -0300

    se corrige ultima consulta incorporada

--- oper_detallada.sql
+++ oper_detallada.sql
@@ -2,13 +2,13 @@
 
 SELECT id_cliente_conta AS "CONTA", 
 	   c.nome AS "NOME CLIENTE", 
-	   COUNT(CASE WHEN id_tipo_transacao = 1 THEN valor END) AS '',
+	   COUNT(CASE WHEN id_tipo_transacao = 1 THEN '' END) AS '',
        SUM(CASE WHEN id_tipo_transacao = 1 THEN valor ELSE 0 END) AS DEPOS,
-	   COUNT(CASE WHEN id_tipo_transacao = 2 THEN valor END) AS '',
+	   COUNT(CASE WHEN id_tipo_transacao = 2 THEN '' END) AS '',
 	   SUM(CASE WHEN id_tipo_transacao = 2 THEN valor ELSE 0 END) AS SAQUES,
-	   COUNT(CASE WHEN id_tipo_transacao = 3 THEN valor END) AS '',
+	   COUNT(CASE WHEN id_tipo_transacao = 3 THEN '' END) AS '',
 	   SUM(CASE WHEN id_tipo_transacao = 3 THEN valor ELSE 0 END) AS 'CONTAS',
-	   COUNT(CASE WHEN id_tipo_transacao = 4 THEN valor END) AS '',
+	   COUNT(CASE WHEN id_tipo_transacao = 4 THEN '' END) AS '',
 	   SUM(CASE WHEN id_tipo_transacao = 4 THEN valor ELSE 0 END) AS TRANSF,
 		
 	   PRINTF('%.2f', (SUM(CASE WHEN id_tipo_transacao = 1 THEN valor
